9dca48a6d73b57dda599a08ed6ccc18d4bd5f4ab
Alexey Shmalko <rasen.dubi@gmail.com>
2013-08-14 15:45:32 +0300

    Some parody multitasking

--- kernel.c
+++ kernel.c
@@ -1,28 +1,32 @@
 #include <uart.h>
 #include <asm.h>
 
-unsigned int first_stack[256];
+unsigned int stacks[2][256];
+unsigned int *tasks[2];
 
 void user_first(void) {
 	uart_puts("In user mode\n");
 	syscall();
 	uart_puts("In user mode again\n");
-	syscall();
-	while(1);
+	while (1) syscall();
+}
+
+void task(void) {
+	uart_puts("In other task\n");
+	while (1) syscall();
 }
 
 int main(void) {
+	tasks[0] = stacks[0] + 256 - 16;
+	tasks[0][0] = 0x10;
+	tasks[0][1] = (unsigned int)&user_first;
 
-	unsigned int *stack;
-	unsigned int *first_stack_start = first_stack + 256 - 16;
-	first_stack_start[0] = 0x10;
-	first_stack_start[1] = (unsigned int)&user_first;
-	uart_puts("Diving into user-mode\n");
-	stack = activate(first_stack_start);
-	uart_puts("In kernel-mode\n");
-	stack = activate(stack);
-	uart_puts("In kernel-mode again\n");
+	tasks[1] = stacks[1] + 256 - 16;
+	tasks[1][0] = 0x10;
+	tasks[1][1] = (unsigned int)&task;
 
+	activate(tasks[0]);
+	activate(tasks[1]);
 
 	while (1);
 	return 0;
