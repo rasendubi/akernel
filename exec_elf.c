631032051d933d35078133a70033a95870568763
Alexey Shmalko <rasen.dubi@gmail.com>
2013-08-24 02:44:31 +0300

    Add ELF program header

--- exec_elf.c
+++ exec_elf.c
@@ -55,3 +55,24 @@ typedef struct Elf32_Ehdr {
 	unsigned short e_shstrndx;
 } Elf32_Ehdr;
 
+/* p_type */
+#define PT_NULL    0
+#define PT_LOAD    1
+#define PT_DYNAMIC 2
+#define PT_INTERP  3
+#define PT_NOTE    4
+#define PT_SHLIB   5
+#define PT_PHDR    6
+#define PT_LOPROC 0x70000000
+#define PT_HIPROC 0x7fffffff
+
+typedef struct Elf32_Phdr {
+	unsigned       p_type;
+	unsigned       p_offset;
+	void *         p_vaddr;
+	void *         p_paddr;
+	unsigned       p_filesz;
+	unsigned       p_memsz;
+	unsigned       p_flags;
+	unsigned       p_align;
+} Elf32_Phdr;
